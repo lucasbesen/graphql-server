38e70e37b8559f3975b0983da49b87a51c496440
lucasbesen <lucassilvabesen@gmail.com>
2018-03-02 21:31:42 -0300

    Add updateSchema

---
+++ scripts/updateSchema.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import { graphql } from 'graphql';
+import { introspectionQuery, printSchema } from 'graphql/utilities';
+import { schema } from '../src/schema';
+
+(async () => {
+  let result = await (graphql(schema, introspectionQuery));
+  if (result.errors) {
+    console.error(
+      'ERROR introspecting schema: ',
+      JSON.stringify(result.errors, null, 2)
+    );
+  } else {
+    fs.writeFileSync(
+      path.join(__dirname, '../data/schema.json'),
+      JSON.stringify(result, null, 2)
+    );
+  }
+})();
+
+fs.writeFileSync(
+  path.join(__dirname, '../data/schema.graphql'),
+  printSchema(schema)
+);
